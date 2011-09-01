86e2669a9f870d7d5f1e50288267d1b7603c42d7
Stian Almås <stian.almaas@gmail.com>
2011-09-01 23:44:50 +0200

    Brute force...

--- src/problem9.clj
+++ src/problem9.clj
@@ -5,7 +5,7 @@
 
 (pythagorean? 3 4 5)
 
-(for [a (range 998) b (range a) c (range b)
+(for [a (range 500) b (range 500) c (range 500)
       :when (and (= 1000 (+ a b c)) (pythagorean? a b c))]
   (list a b c))
 
